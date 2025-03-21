c30eb5f24a341c8438d232141cf1e7a50fb53193
Hugo G <66999296+HGIprojects@users.noreply.github.com>
2025-03-21 11:51:03 +0900

    Add files via upload

---
+++ main/java/com/myweb/mappers/QueryMapper.java
@@ -0,0 +1,14 @@
+package com.myweb.mappers;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QueryMapper{
+
+	Integer visitCounter(
+			
+			);
+	
+	Integer returnId(
+			);
+}
